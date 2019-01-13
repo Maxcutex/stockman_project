e18d555dc1f1da99d9b96b14f1a7e7cf8b9a1ebd
Eno Bassey <ennyboy@gmail.com>
2019-01-14 00:09:26 +0100

    add  database configuration

--- stockman_project/settings/development.py
+++ stockman_project/settings/development.py
@@ -14,13 +14,13 @@ NOSE_ARGS = [
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': 'marketsmith_db',
-		'USER': 'marketsmith_user',
-		'PASSWORD': 'olubanke@1',
-		'HOST': 'localhost',
-		'PORT': '5432',
+		'NAME': os.environ.get('DATABASE_NAME', 'marketsmith_db'),
+        'USER': os.environ.get('DATABASE_USER', 'marketsmith_user'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'olubanke@1'),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
 		'TEST': {
-			'NAME': 'marketsmith_db_test',
+			'NAME': os.environ.get('DATABASE_NAME_TEST', 'marketsmith_db_test'),
 		},
 	}
 }
