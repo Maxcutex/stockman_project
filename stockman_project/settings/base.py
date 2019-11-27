ed51fd187e40a3c1690d16798b7b1a7403ca9ef1
Eno Bassey <ennyboy@gmail.com>
2019-11-27 11:09:40 +0100

    add hosting permission

--- stockman_project/settings/base.py
+++ stockman_project/settings/base.py
@@ -33,6 +33,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '21)^n6=5y$ams&oyqlumbhbaqthx9y+)p=9_&f30tgb-$uibhu'
 #SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
