35df34a4815ab768a6b57791e585745aba951ab6
Eno Bassey <ennyboy@gmail.com>
2019-11-27 15:39:06 +0100

    create default user when deploying

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,5 +6,6 @@ echo "Waiting for postgres..."
 #done
 #echo "PostgreSQL started"
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@stockman.com', 'pass')" | ./manage.py shell
 python manage.py runserver 0.0.0.0:8000
 exec "$@"
