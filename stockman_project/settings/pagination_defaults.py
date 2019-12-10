9c9b75cfbe2db79fbefb077dd1d41fac3c8a785b
Eno Bassey <ennyboy@gmail.com>
2019-12-10 18:37:49 +0100

    change page size settings

---
+++ stockman_project/settings/pagination_defaults.py
@@ -0,0 +1,10 @@
+"""
+Django rest framework default pagination
+"""
+from rest_framework.pagination import PageNumberPagination
+
+
+class DefaultResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
