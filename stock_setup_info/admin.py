ea0b798c922f47a5963df2f130157d749f451032
Eno Bassey <ennyboy@gmail.com>
2019-12-12 19:09:11 +0100

    add sectors to admin

--- stock_setup_info/admin.py
+++ stock_setup_info/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Industry, Stock, Structure, StructureType, StockManagement
+from .models import Industry, Stock, Structure, StructureType, StockManagement, SubSector, MainSector
+
 # Register your models here.
 
 admin.site.register(Industry)
@@ -7,3 +8,5 @@ admin.site.register(Stock)
 admin.site.register(Structure)
 admin.site.register(StructureType)
 admin.site.register(StockManagement)
+admin.site.register(MainSector)
+admin.site.register(SubSector)
