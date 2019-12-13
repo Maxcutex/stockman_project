a2b3170fcd8047a555b0d604e75c22383db08686
Eno Bassey <ennyboy@gmail.com>
2019-12-14 00:14:42 +0100

    fix s3 to work with app

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,6 +6,7 @@ echo "Waiting for postgres..."
 #done
 #echo "PostgreSQL started"
 python manage.py migrate
+python manage.py collectstatic --no-input
 python manage.py initadmin
 python manage.py seed --mode refresh
 python manage.py runserver 0.0.0.0:8000
