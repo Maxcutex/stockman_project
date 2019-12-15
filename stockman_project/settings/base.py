d2e80a6c4eb9965a0acc4534ff3e8c77bccfecb9
Eno Bassey <ennyboy@gmail.com>
2019-12-15 10:42:05 +0100

    set custom s3 url to allowed hosts

--- stockman_project/settings/base.py
+++ stockman_project/settings/base.py
@@ -194,7 +194,7 @@ if USE_S3:
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     CKEDITOR_BASEPATH = f'{STATIC_URL}ckeditor/ckeditor/'
-
+    ALLOWED_HOSTS += [AWS_S3_CUSTOM_DOMAIN]
 
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
