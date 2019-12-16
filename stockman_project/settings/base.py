6c50a03fba6163fe149ea50315fa5c1e226f71ad
Eno Bassey <ennyboy@gmail.com>
2019-12-16 08:30:08 +0100

    set acl property on aws setup

--- stockman_project/settings/base.py
+++ stockman_project/settings/base.py
@@ -185,7 +185,7 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = 'public-write'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     TEXT_CKEDITOR_BASE_PATH = f'https://{AWS_S3_CUSTOM_DOMAIN}/djangocms_text_ckeditor/ckeditor/'
