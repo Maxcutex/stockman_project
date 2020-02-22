c6267b90314b62dfab03e38c1b84582369144f58
Eno Bassey <ennyboy@gmail.com>
2020-02-22 17:57:33 +0100

    fix search document for news: :

--- search_indexes/documents/news.py
+++ search_indexes/documents/news.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django_elasticsearch_dsl import DocType, Index, fields
+from django_elasticsearch_dsl import Document, Index, fields
 from django_elasticsearch_dsl.registries import registry
 from elasticsearch_dsl import analyzer
 
@@ -22,8 +22,8 @@ html_strip = analyzer(
 )
 
 
-@INDEX.doc_type
-class NewsDocument(DocType):
+@registry.register_document
+class NewsDocument(Document):
     """News Elasticsearch document."""
 
     class Index:
@@ -37,68 +37,68 @@ class NewsDocument(DocType):
         model = News  # The model associated with this Document
 
         # The fields of the model you want to be indexed in Elasticsearch
-        # fields = [
-        #     'id',
-        #     'title',
-        #     # 'content',
-        #     'news_date',
-        #     'entry_date',
-        #     # 'stock',
-        #     # 'author',
-        #     # 'sec_code',
-        #     'is_featured',
-        #     'is_main',
-        #     'has_downloadable',
-        #
-        # ]
-    id = fields.IntegerField(attr='id')
+        fields = [
+            'id',
+            'title',
+            # 'content',
+            'news_date',
+            'entry_date',
+            # 'stock',
+            # 'author',
+            # 'sec_code',
+            'is_featured',
+            'is_main',
+            'has_downloadable',
 
-    title = fields.TextField(
-        analyzer=html_strip,
-        fields={
-            'raw': fields.KeywordField(),
-        }
-    )
-
-    content = fields.TextField(
-        analyzer=html_strip,
-        fields={
-            'raw': fields.KeywordField(),
-        }
-    )
-    news_date = fields.DateField()
-    entry_date = fields.DateField()
-
-    stock = fields.TextField(
-        attr='stock_indexing',
-        analyzer=html_strip,
-        fields={
-            'raw': fields.KeywordField(),
-        }
-    )
-
-    author = fields.TextField(
-        attr='author_indexing',
-        analyzer=html_strip,
-        fields={
-            'raw': fields.KeywordField(),
-        }
-    )
-
-    sec_code = fields.TextField(
-        analyzer=html_strip,
-        fields={
-            'raw': fields.KeywordField(),
-        }
-    )
-
-    is_featured = fields.BooleanField()
-
-    has_downloadable = fields.BooleanField()
-
-    is_main = fields.BooleanField()
-
-    class Meta(object):
-        """Meta options."""
-
-        model = News  # The model associate with this DocType
+        ]
+    # id = fields.IntegerField(attr='id')
+    #
+    # title = fields.TextField(
+    #     analyzer=html_strip,
+    #     fields={
+    #         'raw': fields.KeywordField(),
+    #     }
+    # )
+    #
+    # content = fields.TextField(
+    #     analyzer=html_strip,
+    #     fields={
+    #         'raw': fields.KeywordField(),
+    #     }
+    # )
+    # news_date = fields.DateField()
+    # entry_date = fields.DateField()
+    #
+    # stock = fields.TextField(
+    #     attr='stock_indexing',
+    #     analyzer=html_strip,
+    #     fields={
+    #         'raw': fields.KeywordField(),
+    #     }
+    # )
+    #
+    # author = fields.TextField(
+    #     attr='author_indexing',
+    #     analyzer=html_strip,
+    #     fields={
+    #         'raw': fields.KeywordField(),
+    #     }
+    # )
+    #
+    # sec_code = fields.TextField(
+    #     analyzer=html_strip,
+    #     fields={
+    #         'raw': fields.KeywordField(),
+    #     }
+    # )
+    #
+    # is_featured = fields.BooleanField()
+    #
+    # has_downloadable = fields.BooleanField()
+    #
+    # is_main = fields.BooleanField()
+    #
+    # class Meta(object):
+    #     """Meta options."""
+    #
+    #     model = News  # The model associate with this DocType
