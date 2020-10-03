a52331936685dd0cce430bc85c8a75fd8e25f5a5
Eno Bassey <ennyboy@gmail.com>
2020-10-04 00:05:22 +0100

    add migration for pricetemp

--- search_indexes/signal.py
+++ search_indexes/signal.py
@@ -6,49 +6,49 @@ from django_elasticsearch_dsl.registries import registry
 
 @receiver(post_save)
 def update_document(sender, **kwargs):
-	"""
-	Update document on added/changed records.
+    """
+    Update document on added/changed records.
     """
 
-	app_label = sender._meta.app_label
-	model_name = sender._meta.model_name
-	instance = kwargs['instance']
+    app_label = sender._meta.app_label
+    model_name = sender._meta.model_name
+    instance = kwargs["instance"]
 
-	if app_label == 'news':
-		# If it is `news.stock` that is being updated.
-		if model_name == 'stock':
-			instances = instance.news.all()
-			for _instance in instances:
-				registry.update(_instance)
+    if app_label == "news":
+        # If it is `news.stock` that is being updated.
+        if model_name == "stock":
+            instances = instance.news.all()
+            for _instance in instances:
+                registry.update(_instance)
 
-		# If it is `news.Author` that is being updated.
-		if model_name == 'author':
-			instances = instance.news.all()
-			for _instance in instances:
-				registry.update(_instance)
+        # If it is `news.Author` that is being updated.
+        if model_name == "author":
+            instances = instance.news.all()
+            for _instance in instances:
+                registry.update(_instance)
 
 
 @receiver(post_delete)
 def delete_document(sender, **kwargs):
-	"""
-	Update document on deleted records.
+    """
+    Update document on deleted records.
     """
 
-	app_label = sender._meta.app_label
-	model_name = sender._meta.model_name
-	instance = kwargs['instance']
+    app_label = sender._meta.app_label
+    model_name = sender._meta.model_name
+    instance = kwargs["instance"]
 
-	if app_label == 'news':
-		# If it is `news.stock` that is being updated.
-		if model_name == 'stock':
-			instances = instance.news.all()
-			for _instance in instances:
-				registry.update(_instance)
-				registry.delete(_instance, raise_on_error=False)
+    if app_label == "news":
+        # If it is `news.stock` that is being updated.
+        if model_name == "stock":
+            instances = instance.news.all()
+            for _instance in instances:
+                registry.update(_instance)
+                registry.delete(_instance, raise_on_error=False)
 
-		# If it is `books.Author` that is being updated.
-		if model_name == 'author':
-			instances = instance.news.all()
-			for _instance in instances:
-				registry.update(_instance)
-				registry.delete(_instance, raise_on_error=False)
+        # If it is `books.Author` that is being updated.
+        if model_name == "author":
+            instances = instance.news.all()
+            for _instance in instances:
+                registry.update(_instance)
+                registry.delete(_instance, raise_on_error=False)
--- stock_maintain/models.py
+++ stock_maintain/models.py
@@ -23,7 +23,8 @@ class PriceList(models.Model):
     volume = models.FloatField()
     x_value = models.FloatField()
     stock = models.ForeignKey(
-        Stock, on_delete=models.CASCADE, related_name='price_stock', null=True)
+        Stock, on_delete=models.CASCADE, related_name="price_stock", null=True
+    )
 
     def __str__(self):
         return self.sec_code
@@ -36,7 +37,8 @@ class PriceList(models.Model):
 
 class Quote(models.Model):
     stock_id = models.ForeignKey(
-        Stock, on_delete=models.CASCADE, related_name='quotes_stock', null=True)
+        Stock, on_delete=models.CASCADE, related_name="quotes_stock", null=True
+    )
     sec_code = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     contact = models.CharField(max_length=100)
@@ -62,7 +64,8 @@ class AsiIndex(models.Model):
 
 class BonusTracker(models.Model):
     stock_id = models.ForeignKey(
-        Stock, on_delete=models.CASCADE, related_name='bonus_stock', null=True)
+        Stock, on_delete=models.CASCADE, related_name="bonus_stock", null=True
+    )
     sec_code = models.CharField(max_length=10)
     bonus_val = models.SmallIntegerField()
     bonus_aggregate = models.SmallIntegerField()
@@ -98,10 +101,10 @@ class ImageSizeType(ChoiceEnum):
 
 class Dividend(models.Model):
     stock_id = models.ForeignKey(
-        Stock, on_delete=models.CASCADE, related_name='dividend_stock', null=True)
+        Stock, on_delete=models.CASCADE, related_name="dividend_stock", null=True
+    )
     sec_code = models.CharField(max_length=10)
-    quarter_type = EnumChoiceField(
-        QuarterType, default=QuarterType.first_quarter)
+    quarter_type = EnumChoiceField(QuarterType, default=QuarterType.first_quarter)
     interim = models.FloatField()
     bonus = models.FloatField()
 
@@ -134,13 +137,19 @@ class News(models.Model):
     news_date = models.DateField()
     entry_date = models.DateField()
     stock = models.ForeignKey(
-        Stock, on_delete=models.CASCADE, related_name='news_stock', null=True)
+        Stock, on_delete=models.CASCADE, related_name="news_stock", null=True
+    )
     sec_code = models.CharField(max_length=10, null=True)
     is_featured = models.BooleanField()
     has_downloadable = models.BooleanField(default=False)
     is_main = models.BooleanField(default=False)
     author = models.ForeignKey(
-        SiteAuthor, on_delete=models.CASCADE, related_name='news_author', null=True, blank=True)
+        SiteAuthor,
+        on_delete=models.CASCADE,
+        related_name="news_author",
+        null=True,
+        blank=True,
+    )
 
     def get_summary(self, char):
         return self.content[:char]
@@ -163,18 +172,22 @@ class News(models.Model):
     def author_indexing(self):
         """Publisher for indexing. Used in Elasticsearch indexing. """
         if self.author is not None:
-            return self.author.first_name + ' ' + self.author.last_name
+            return self.author.first_name + " " + self.author.last_name
 
 
 class NewsImage(models.Model):
-    image_choice = Choices('size930x620', 'size450x330', 'size300x200', 'size330x220', 'size158x158')
+    image_choice = Choices(
+        "size930x620", "size450x330", "size300x200", "size330x220", "size158x158"
+    )
     news = models.ForeignKey(
-        News, on_delete=models.CASCADE, related_name='visual_news', null=True)
+        News, on_delete=models.CASCADE, related_name="visual_news", null=True
+    )
     is_main = models.BooleanField()
     image_file = models.ImageField(blank=True, upload_to="images/news_image")
     name = models.CharField(max_length=100)
     image_type = models.CharField(
-        choices=image_choice, default=image_choice.size930x620, max_length=30)
+        choices=image_choice, default=image_choice.size930x620, max_length=30
+    )
 
     def __str__(self):
         return self.name
@@ -186,14 +199,16 @@ class NewsImage(models.Model):
 
 
 class NewsFile(models.Model):
-    doc_choices = Choices('pdf', 'word', 'excel')
+    doc_choices = Choices("pdf", "word", "excel")
     news = models.ForeignKey(
-        News, on_delete=models.CASCADE, related_name='doc_news', null=True)
+        News, on_delete=models.CASCADE, related_name="doc_news", null=True
+    )
     is_main = models.BooleanField()
     doc_file = models.FileField(blank=True, upload_to="files/news_docs")
     name = models.CharField(max_length=100)
     doc_type = models.CharField(
-        choices=doc_choices, default=doc_choices.pdf, max_length=30)
+        choices=doc_choices, default=doc_choices.pdf, max_length=30
+    )
 
     def __str__(self):
         return self.name
@@ -221,12 +236,11 @@ class OfferMethod(ChoiceEnum):
 class OfferIpo(models.Model):
     company_name = models.CharField(max_length=150)
     country_id = models.ForeignKey(
-        Structure, on_delete=models.CASCADE, related_name='country', null=True)
+        Structure, on_delete=models.CASCADE, related_name="country", null=True
+    )
     exchange_code = models.CharField(max_length=10)
-    offer_type = EnumChoiceField(
-        OfferType, default=OfferType.ipo)
-    quarter_type = EnumChoiceField(
-        OfferMethod, default=OfferMethod.sal)
+    offer_type = EnumChoiceField(OfferType, default=OfferType.ipo)
+    quarter_type = EnumChoiceField(OfferMethod, default=OfferMethod.sal)
     open_date = models.DateField(null=True)
     close_date = models.DateField(null=True)
     extended_date = models.DateField(null=True)
@@ -239,13 +253,17 @@ class OfferIpo(models.Model):
 
 class NewsCategorySection(models.Model):
     news = models.ForeignKey(
-        News, on_delete=models.CASCADE, related_name='category_news', null=True)
+        News, on_delete=models.CASCADE, related_name="category_news", null=True
+    )
     section_category = models.ForeignKey(
-        SectionGroup, on_delete=models.CASCADE, related_name='category_news_section', null=True)
+        SectionGroup,
+        on_delete=models.CASCADE,
+        related_name="category_news_section",
+        null=True,
+    )
 
     def __str__(self):
         return self.section_category.section_name
-
 
     @classmethod
     def truncate(cls):
@@ -259,7 +277,8 @@ class AnalysisOpinion(models.Model):
     opinion_date = models.DateField()
     entry_date = models.DateField()
     author = models.ForeignKey(
-        SiteAuthor, on_delete=models.CASCADE, related_name='analysis_author', null=True)
+        SiteAuthor, on_delete=models.CASCADE, related_name="analysis_author", null=True
+    )
 
     @classmethod
     def truncate(cls):
@@ -268,14 +287,19 @@ class AnalysisOpinion(models.Model):
 
 
 class AnalysisFile(models.Model):
-    doc_choices = Choices('pdf', 'word', 'excel')
+    doc_choices = Choices("pdf", "word", "excel")
     analysis = models.ForeignKey(
-        AnalysisOpinion, on_delete=models.CASCADE, related_name='doc_analysis', null=True)
+        AnalysisOpinion,
+        on_delete=models.CASCADE,
+        related_name="doc_analysis",
+        null=True,
+    )
     is_main = models.BooleanField()
     doc_file = models.FileField(blank=True, upload_to="files/analysis_docs")
     name = models.CharField(max_length=100)
     doc_type = models.CharField(
-        choices=doc_choices, default=doc_choices.pdf, max_length=30)
+        choices=doc_choices, default=doc_choices.pdf, max_length=30
+    )
 
     def __str__(self):
         return self.name
@@ -288,9 +312,17 @@ class AnalysisFile(models.Model):
 
 class AnalysisCategorySection(models.Model):
     analysis = models.ForeignKey(
-        AnalysisOpinion, on_delete=models.CASCADE, related_name='category_analysis', null=True)
+        AnalysisOpinion,
+        on_delete=models.CASCADE,
+        related_name="category_analysis",
+        null=True,
+    )
     section_category = models.ForeignKey(
-        SectionGroup, on_delete=models.CASCADE, related_name='category_analysis_section', null=True)
+        SectionGroup,
+        on_delete=models.CASCADE,
+        related_name="category_analysis_section",
+        null=True,
+    )
 
     @classmethod
     def truncate(cls):
@@ -299,14 +331,21 @@ class AnalysisCategorySection(models.Model):
 
 
 class AnalysisImage(models.Model):
-    image_choice = Choices('size930x620', 'size450x330', 'size300x200', 'size330x220', 'size158x158')
+    image_choice = Choices(
+        "size930x620", "size450x330", "size300x200", "size330x220", "size158x158"
+    )
     analysis = models.ForeignKey(
-        AnalysisOpinion, on_delete=models.CASCADE, related_name='visual_analysis', null=True)
+        AnalysisOpinion,
+        on_delete=models.CASCADE,
+        related_name="visual_analysis",
+        null=True,
+    )
     is_main = models.BooleanField()
     image_file = models.ImageField(blank=True, upload_to="images/analysis_image")
     name = models.CharField(max_length=100)
     image_type = models.CharField(
-        choices=image_choice, default=image_choice.size930x620, max_length=30)
+        choices=image_choice, default=image_choice.size930x620, max_length=30
+    )
 
     def __str__(self):
         return self.name
@@ -323,7 +362,11 @@ class InsideBusiness(models.Model):
     opinion_date = models.DateField()
     entry_date = models.DateField()
     author = models.ForeignKey(
-        SiteAuthor, on_delete=models.CASCADE, related_name='inside_business_author', null=True)
+        SiteAuthor,
+        on_delete=models.CASCADE,
+        related_name="inside_business_author",
+        null=True,
+    )
 
     def __str__(self):
         return self.title
@@ -336,9 +379,17 @@ class InsideBusiness(models.Model):
 
 class InsideBusinessSection(models.Model):
     inside_business = models.ForeignKey(
-        InsideBusiness, on_delete=models.CASCADE, related_name='category_inside_business', null=True)
+        InsideBusiness,
+        on_delete=models.CASCADE,
+        related_name="category_inside_business",
+        null=True,
+    )
     section_category = models.ForeignKey(
-        SectionGroup, on_delete=models.CASCADE, related_name='category_inside_business_section', null=True)
+        SectionGroup,
+        on_delete=models.CASCADE,
+        related_name="category_inside_business_section",
+        null=True,
+    )
 
     @classmethod
     def truncate(cls):
@@ -347,14 +398,19 @@ class InsideBusinessSection(models.Model):
 
 
 class InsideBusinessFile(models.Model):
-    doc_choices = Choices('pdf', 'word', 'excel')
+    doc_choices = Choices("pdf", "word", "excel")
     inside_business = models.ForeignKey(
-        InsideBusiness, on_delete=models.CASCADE, related_name='doc_inside_business', null=True)
+        InsideBusiness,
+        on_delete=models.CASCADE,
+        related_name="doc_inside_business",
+        null=True,
+    )
     is_main = models.BooleanField()
     doc_file = models.FileField(blank=True, upload_to="files/inside_business_docs")
     name = models.CharField(max_length=100)
     doc_type = models.CharField(
-        choices=doc_choices, default=doc_choices.pdf, max_length=30)
+        choices=doc_choices, default=doc_choices.pdf, max_length=30
+    )
 
     def __str__(self):
         return self.name
@@ -366,14 +422,21 @@ class InsideBusinessFile(models.Model):
 
 
 class InsideBusinessImage(models.Model):
-    image_choice = Choices('size930x620', 'size450x330', 'size300x200', 'size330x220', 'size158x158')
+    image_choice = Choices(
+        "size930x620", "size450x330", "size300x200", "size330x220", "size158x158"
+    )
     inside_business = models.ForeignKey(
-        InsideBusiness, on_delete=models.CASCADE, related_name='visual_inside_business', null=True)
+        InsideBusiness,
+        on_delete=models.CASCADE,
+        related_name="visual_inside_business",
+        null=True,
+    )
     is_main = models.BooleanField()
     image_file = models.ImageField(blank=True, upload_to="images/inside_business_image")
     name = models.CharField(max_length=100)
     image_type = models.CharField(
-        choices=image_choice, default=image_choice.size930x620, max_length=30)
+        choices=image_choice, default=image_choice.size930x620, max_length=30
+    )
 
     def __str__(self):
         return self.name
@@ -394,9 +457,11 @@ class GeneratedAnalysisDate(models.Model):
     date_generated_for = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class PriceAnalysisTemp(models.Model):
     stock = models.ForeignKey(
-        Stock, on_delete=models.CASCADE, related_name='temp_analysi_stock', null=True)
+        Stock, on_delete=models.CASCADE, related_name="temp_analysi_stock", null=True
+    )
     sec_code = models.CharField(max_length=100)
     price = models.FloatField()
     min_year = models.FloatField(null=True)
@@ -418,6 +483,4 @@ class PriceAnalysisTemp(models.Model):
     six_months_cent = models.FloatField(null=True)
     one_year_cent = models.FloatField(null=True)
     price_date = models.DateTimeField(null=True)
-
-
-
+    price_year_to_date_cent = models.FloatField(null=True)
