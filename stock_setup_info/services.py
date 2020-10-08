f6350b652e555dc726c359f110c4b55ef39852c1
Eno Bassey <ennyboy@gmail.com>
2020-10-08 23:21:53 +0100

    modify result of by code

--- stock_setup_info/services.py
+++ stock_setup_info/services.py
@@ -7,19 +7,19 @@ def get_stock_by_code(query_params):
     """ Get stock by the stock code provided"""
 
     try:
-        stock_code = query_params.get('stock_code')
-    except:
-        raise APIException(detail='Provide proper stock code')
-    return Stock.objects.filter(
-        stock_code=stock_code
-    )
+        stock_code = query_params.get("stock_code")
+    except Exception:
+        raise APIException(detail="Provide proper stock code")
+    return Stock.objects.filter(stock_code=stock_code).first()
 
 
 def stock_search_like_name(query_params):
     """ Get all stock with like search of name provided"""
 
     try:
-        stock_code = query_params.get('stock_code')
-    except:
-        raise APIException(detail='Provide proper search variable')
-    return Stock.objects.filter(Q(stock_code__contains=stock_code) | Q(name__contains=stock_code))
+        stock_code = query_params.get("stock_code")
+    except Exception:
+        raise APIException(detail="Provide proper search variable")
+    return Stock.objects.filter(
+        Q(stock_code__contains=stock_code) | Q(name__contains=stock_code)
+    )
