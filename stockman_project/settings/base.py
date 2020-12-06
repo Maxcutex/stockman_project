5c22df61e37eb9b96af7343dbd85b15128d2673b
Eno Bassey <eno.bassey@sevenrooms.com>
2020-12-06 08:54:28 +0100

    add REDIS_URL

--- stockman_project/settings/base.py
+++ stockman_project/settings/base.py
@@ -379,7 +379,9 @@ CACHE_TTL = 60 * 1  # 1 minute
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get("j", "localhost"),  # "redis://127.0.0.1:6379/0",
+        "LOCATION": os.environ.get(
+            "REDIS_URL", "localhost"
+        ),  # "redis://127.0.0.1:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
